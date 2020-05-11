8ddd2d571631752f6133c9ef20c232a9b2f421f4
glevon <levon77777@gmai.com>
2020-05-11 22:35:27 +0400

    update1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import {BrowserRouter,Route,Link} from "react-router-dom"
 import './App.css';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Signup from './components/Signup';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <nav className="navbar navbar-expand-sm bg-light">
+         <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/" >Singnup</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login" >Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/profile" >Profile</Link>
+          </li>
+        </ul>
+        </nav>
+        <Route path="/" exact component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route path="/profile" component={Profile} />
+
+      </BrowserRouter>
+    )
+  }
 }
 
-export default App;
+export default App
+
